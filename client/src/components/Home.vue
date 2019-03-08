c9e38bfa42cc49444dd6b1ac84695e3c39b62235
vinnadipo09 <vinnadipo09@gmail.com>
2019-03-08 13:07:41 +0300

    complete setup of server and client

---
+++ client/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+      <div class="home-parallax">
+          <v-parallax src="/static/page3.png">
+            <div class="home-text">
+                <h1>ViadTechnologies</h1>
+                <h3>The Future Predetermined</h3>
+            </div>
+          </v-parallax>
+      </div>
+      <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-parallax{
+    margin-top: 60px;
+}
+.home-text{
+    margin-top: 400px;
+    /* margin-right: 10px; */
+}
+</style>
---
+++ client/src/components/sharedComponents/PageHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-toolbar fixed class="brown" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>ViadTechnologies</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat outline color="green" class="my-menus">Viad Center</v-btn>
+        <v-btn flat class="my-menus">Home</v-btn>
+        <v-btn flat class="my-menus">web Services</v-btn>
+        <v-btn flat class="my-menus">Mobile Services</v-btn>
+        <v-btn flat class="my-menus">Engineering Services</v-btn>
+        <v-btn flat class="my-menus">Consultancy</v-btn>
+        <v-btn flat class="my-menus">Online Store</v-btn>
+        <v-btn flat class="my-menus">Promotion and Offers</v-btn>
+        <v-btn flat class="my-menus">About Us</v-btn>
+        <v-btn flat class="my-menus">Register</v-btn>
+        <v-btn flat class="my-menus">Log In</v-btn>
+        <v-btn flat class="my-menus">Log Out</v-btn>
+
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  name: 'PageHeader',
+  data () {
+    return {
+    // information
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-menus{
+    text-transform: none !important;
+}
+.my-menus:hover {
+    text-decoration: underline;
+}
+</style>
